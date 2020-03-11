8bcafc96fd474718a4d1978168cffdad2b244a1a
jpbandeiralima <jpbandeiralima@gmail.com>
2020-03-11 20:43:48 -0300

    versão de desenvolvimento

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -7,23 +7,49 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Analyzer {
-    private String input = "(-423 - -12 - -16 * 2 ** 2 / 2)";
     private Token token = new Token();
+
+    private String input = "(-423 - -12 - -16 * 2 ** 2 / 2)";
+    String noSpaceArray = input.replaceAll("\\s*", "");
+
+    String regexNumber = "[0-9]";
+    String regexAllNumber = "[0-9]*";
+
     private List<String> array = new ArrayList<>();
 
+    Pattern pattern = null;
+    Matcher matcher = null;
 
+    public boolean isNumber(String regexNumber, String input){
+        pattern = Pattern.compile(regexNumber);
+        matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getNumber(String regexNumber, String input){
+        pattern = Pattern.compile(regexNumber);
+        matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+            return matcher.group();
+        }
+            return "";
+
+    }
 
     public void analyze(){
 
-        String noSpaceArray = input.replaceAll("\\s*", "");
-        Pattern pattern = Pattern.compile("[0-9]");
-        Matcher matcher = pattern.matcher(noSpaceArray);
-
-
-
-        while (matcher.find())
-        System.out.println(matcher.group());
-        
+        while (true) {
+            if (isNumber(regexNumber, noSpaceArray)) {
+                String teste = getNumber(regexAllNumber, noSpaceArray);
+                System.out.println(teste);
+            }
+        }
         //mostrar();
     }
 
