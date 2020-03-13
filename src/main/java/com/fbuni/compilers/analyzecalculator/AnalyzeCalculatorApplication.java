81a40c0921440b8a7df9b31a7b4fc2527704bc32
jpbandeiralima <jpbandeiralima@gmail.com>
2020-03-13 01:58:25 -0300

    Projeto base

---
+++ src/main/java/com/fbuni/compilers/analyzecalculator/AnalyzeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fbuni.compilers.analyzecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyzeCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fbuni/compilers/analyzecalculator/AnalyzeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbuni.compilers.analyzecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzeCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
