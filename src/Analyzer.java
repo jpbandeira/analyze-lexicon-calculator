2fc6bba4abf3e75359c5f2cffef608b22d5830fe
jpbandeiralima <jpbandeiralima@gmail.com>
2020-03-13 00:09:02 -0300

    Possivel versão final

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,87 +1,86 @@
-import jdk.nashorn.internal.runtime.regexp.RegExp;
-import jdk.nashorn.internal.runtime.regexp.RegExpMatcher;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Analyzer {
-    private Token token = new Token();
-
-    private String input = "(-423 - -12 - -16 * 2 ** 2 / 2)";
-    String noSpaceArray = input.replaceAll("\\s*", "");
-
-    String regexNumber = "[0-9]";
-    String regexAllNumber = "[0-9]*";
-
+    private String input = "((-423 + 2)--12+-16*2**2/2)";
+    //private Token tokenPrincipal = new Token();
+    private Token token = null;
     private List<String> array = new ArrayList<>();
+    private List<Token> resultado = new ArrayList<>();
 
-    Pattern pattern = null;
-    Matcher matcher = null;
 
-    public boolean isNumber(String regexNumber, String input){
-        pattern = Pattern.compile(regexNumber);
-        matcher = pattern.matcher(input);
-
-        while (matcher.find()) {
-            return true;
-        }
-        return false;
-    }
-
-    public String getNumber(String regexNumber, String input){
-        pattern = Pattern.compile(regexNumber);
-        matcher = pattern.matcher(input);
-
-        while (matcher.find()) {
-            System.out.println(matcher.group());
-            return matcher.group();
-        }
-            return "";
-
-    }
 
     public void analyze(){
+        String noSpaceArray = input.replaceAll("\\s*", "");
+        Pattern pattern = Pattern.compile("[-]?[0-9]+|[-]|[+]|[*]*");
+        Matcher matcher = pattern.matcher(noSpaceArray);
+        String[] matchers = pattern.split(noSpaceArray);
 
-        while (true) {
-            if (isNumber(regexNumber, noSpaceArray)) {
-                String teste = getNumber(regexAllNumber, noSpaceArray);
-                System.out.println(teste);
-            }
-        }
-        //mostrar();
+        while (matcher.find())
+            if(!matcher.group().equals(""))
+                array.add(matcher.group());
+
+        for(String valor:matchers)
+            if(!valor.equals(""))
+                array.add(valor);
+        verificarValor();
+        mostrar();
     }
 
     public void mostrar(){
-        for(String valor:array){
-            System.out.println(valor);
+        for(Token valor:resultado){
+            System.out.println(valor.getLexama()+" "+valor.getTipo()+" "+valor.getValor());
         }
     }
 
-    public void verificarValor(String valor){
-        switch (valor){
-            case "+":
-                token.setValor("soma");
-                break;
-            case "-":
-                token.setValor("subtracao");
-                break;
-            case "/":
-                token.setValor("divisao");
-                break;
-            case "*":
-                token.setValor("multiplicacao");
-                break;
-            case "**":
-                token.setValor("exponenciacao");
-                break;
-            case "(":
-                token.setValor("parenteseEsquerdo");
-                break;
-            case ")":
-                token.setValor("parenteseDireito");
-                break;
+    public void verificarValor(){
+        for(String valorDoArray:array) {
+            switch (valorDoArray) {
+                case "+":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("operador");
+                    token.setValor("soma");
+                    resultado.add(token);
+                    break;
+                case "-":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("operador");
+                    token.setValor("subtracao");
+                    resultado.add(token);
+                    break;
+                case "*":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("operador");
+                    token.setValor("multiplicação");
+                    resultado.add(token);
+                    break;
+                case "**":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("operador");
+                    token.setValor("exponenciação");
+                    resultado.add(token);
+                    break;
+                case "(":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("pontuação");
+                    token.setValor("parenteseDireito");
+                    resultado.add(token);
+                    break;
+                case ")":
+                    token = new Token();
+                    token.setLexama(valorDoArray);
+                    token.setTipo("pontuação");
+                    token.setValor("parenteseEsquerdo");
+                    resultado.add(token);
+                    break;
+            }
         }
     }
 }
