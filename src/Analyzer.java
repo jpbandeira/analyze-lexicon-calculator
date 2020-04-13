2a9035b9dfdac16609d450b5d196906fb8089bab
jpbandeiralima <jpbandeiralima@gmail.com>
2020-04-12 22:32:22 -0300

    Refatoração de lançamento da exceção para caractere invalido

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -6,55 +6,61 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Analyzer {
-     /*"((-423 + 2) - (-12+ -16)/67 * (2**2)/2)"*/
+    /*"((-423 + 2) - (-12+ -16)/67 * (2**2)/2)"*/
     private Token token = null;
     private List<String> array = new ArrayList<>();
     private List<Token> result = new ArrayList<>();
 
     Pattern pattern = Pattern.compile("[/]|[)]|[(]|[-]?[0-9]+|[-]|[+]|[*]*");
-    Pattern patternCaracteresInvalidos = Pattern.compile("[A-Z]+|[a-z]+");
+    Pattern patternCaracteresInvalidos = Pattern.compile("[A-Z]|[a-z]");
     String anterior = "";
+    StringBuffer stringBufferCaracteresInvalidos = new StringBuffer();
 
     public void analyzeExpression(String input) throws Exception {
-            String noSpaceArray = input.replaceAll("\\s*", "");
+        String noSpaceArray = input.replaceAll("\\s*", "");
 
-            Matcher matchers = pattern.matcher(noSpaceArray);
-            Matcher matchersCaracteresInvalidos = patternCaracteresInvalidos.matcher(noSpaceArray);
+        Matcher matchers = pattern.matcher(noSpaceArray);
+        Matcher matchersCaracteresInvalidos = patternCaracteresInvalidos.matcher(noSpaceArray);
 
-            if (!matchersCaracteresInvalidos.find()) {
-                while (matchers.find()){
-                    if (!matchers.group().equals("")) {
-                        array.add(matchers.group());
-                    }
+        if (!matchersCaracteresInvalidos.find()) {
+            while (matchers.find()) {
+                if (!matchers.group().equals("")) {
+                    array.add(matchers.group());
                 }
-            }else{
-                throw new Exception("Caractere Invalido: " + matchersCaracteresInvalidos.group());
             }
+        } else {
+            stringBufferCaracteresInvalidos.append("[ ");
+            while (matchersCaracteresInvalidos.find()) {
+                stringBufferCaracteresInvalidos.append(matchersCaracteresInvalidos.group() + ", ");
+            }
+            stringBufferCaracteresInvalidos.append("]");
+            throw new Exception("Caracteres Invalidos: " + stringBufferCaracteresInvalidos.toString());
+        }
 
-            checkAtributes();
-            showResults();
+        checkAtributes();
+        showResults();
     }
 
-    public void checkAtributes(){
+    public void checkAtributes() {
         StringBuffer stringBuffer = new StringBuffer();
-        for(String value:array) {
+        for (String value : array) {
             char[] valor = value.toCharArray();
-                if (valor.length > 1 && valor[0] == '-' && isNumber(String.valueOf(valor[1])) && !checkValues(anterior) /*!anterior.equals("-")*/) {
-                    int i = 1;
-                    token = new Token(String.valueOf(valor[0]), "operador", "subtracao");
-                    result.add(token);
-                    if(isNumber(String.valueOf(valor[i]))) {
-                        while (i < valor.length) {
-                            char valorChar=valor[i];
-                            stringBuffer.append(valorChar);
-                            i++;
-                        }
-                        String valueStringBuffer = stringBuffer.toString();
-                        token = new Token(valueStringBuffer, "numero", valueStringBuffer);
-                        result.add(token);
-                        stringBuffer = new StringBuffer();
+            if (valor.length > 1 && valor[0] == '-' && isNumber(String.valueOf(valor[1])) && !checkValues(anterior)) {
+                int i = 1;
+                token = new Token(String.valueOf(valor[0]), "operador", "subtracao");
+                result.add(token);
+                if (isNumber(String.valueOf(valor[i]))) {
+                    while (i < valor.length) {
+                        char valorChar = valor[i];
+                        stringBuffer.append(valorChar);
+                        i++;
                     }
-            }else {
+                    String valueStringBuffer = stringBuffer.toString();
+                    token = new Token(valueStringBuffer, "numero", valueStringBuffer);
+                    result.add(token);
+                    stringBuffer = new StringBuffer();
+                }
+            } else {
                 anterior = value;
                 switch (value) {
                     case "(":
@@ -94,21 +100,21 @@ public class Analyzer {
         }
     }
 
-    public boolean isNumber(String value){
+    public boolean isNumber(String value) {
         boolean number = value.matches("[-]?[0-9]+");
         return number ? true : false;
     }
 
-    public void showResults(){
-        for(Token value:result)
+    public void showResults() {
+        for (Token value : result)
             System.out.println(value.toString());
     }
 
-    public boolean checkValues(String value){
+    public boolean checkValues(String value) {
         List<String> list = new ArrayList<>();
-        list.addAll(Arrays.asList("-","(",")","+","*","**","/"));
-        for(String valueOfList:list){
-            if(valueOfList.equals(value)){
+        list.addAll(Arrays.asList("-", "(", ")", "+", "*", "**", "/"));
+        for (String valueOfList : list) {
+            if (valueOfList.equals(value)) {
                 return true;
             }
         }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Analyzer analyzer = new Analyzer();
         Scanner scanner = new Scanner(System.in);
+        String input = "";
         //String input="((-423 + 2) - (-12+ -16)/67 * (2**2)/2)";
         //String input="(42 + (2 - 295))";
-        String input="( (42-2) - (2-2) + (32-25874) )";
+        //String input="( a(42-2) - (2-2) + (32-25874) )";
 
-        //System.out.println("Digite a entrada: ");
-        //input = scanner.nextLine();
+        System.out.println("Digite a entrada: ");
+        input = scanner.nextLine();
         System.out.println(input);
         analyzer.analyzeExpression(input);
     }
